91c38d7353e3973d1e5c0d796e71667ae8907f7c
burak0arslan <burakarslan42@icloud.com>
2022-04-19 21:00:42 -0400

    day 49 Inheritance

---
+++ src/day49_inheritance/Animal.java
@@ -0,0 +1,14 @@
+package day49_inheritance;
+
+import my_utilities.StringUtil;
+
+public class Animal {
+
+    String species;
+    int numberOfLegs;
+
+    public void walk(){
+        System.out.println(species+" is walking");
+    }
+
+}
